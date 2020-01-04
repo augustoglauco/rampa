67a4b9e1f7b17aa19efcd27261b30dcee525d6ba
augustoglauco <augustoglauco@gmail.com>
2020-01-04 16:08:24 -0300

    Add project files.

---
+++ Rampa/Rampa.Android/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android;
+
+namespace Rampa.Droid
+{
+    [Activity(Label = "Rampa", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        readonly string[] PermissionsLocation = {
+                //Manifest.Permission.AccessCoarseLocation,
+                // Manifest.Permission.AccessFineLocation,
+                Manifest.Permission_group.Location,
+                Manifest.Permission_group.Network,
+                Manifest.Permission_group.BluetoothNetwork,
+                Manifest.Permission.Internet
+        };
+
+        const int RequestId = 0;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            RequestPermissions(PermissionsLocation, RequestId);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Rampa/Rampa/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Rampa.Model
+{
+    class Person
+    {
+        public Guid PersonId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
